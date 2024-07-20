a493a83d215b8e92936cf46cb1a6f19396b0c34f
RShnitser <romanshnitser@hotmail.com>
2024-07-19 21:19:25 -0400

    add token types

--- html_parser.js
+++ html_parser.js
@@ -1,3 +1,16 @@
+const TokenType = {
+  INVALID: 0,
+  OPEN_TAG: 1,
+  CLOSE_TAG: 2,
+  CONTENT: 3,
+  END: 4,
+};
+
+function Token(type, value) {
+  this.type = type;
+  this.value = value;
+}
+
 async function fetchUrlText(url) {
   try {
     const response = await fetch(url);
@@ -19,3 +32,24 @@ async function fetchUrlText(url) {
     console.error(e.message);
   }
 }
+
+async function parseHTML(url) {
+  const text = await fetchUrlText(url);
+  console.log(text);
+}
+
+function tokenize(html) {
+  const tokens = [];
+  let cursor = 0;
+
+  while (cursor < html.length) {
+    const current = html[cursor];
+    const next = cursor + 1 === html.length ? 0 : html[cursor + 1];
+
+    if (current === "<" && next !== "/") {
+    } else if (current === "<" && next === "/") {
+    }
+  }
+}
+
+export { parseHTML };
--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 import { argv } from "process";
-
+import { parseHTML } from "./html_parser.js";
 async function main() {
-  if (argv.length < 3) {
-    console.error("please provide a website");
-    return;
-  }
-
-  if (argv.lengh > 4) {
-    console.error("too many arguments");
-    return;
-  }
-
-  const baseURL = argv[2];
+  // if (argv.length < 3) {
+  //   console.error("please provide a website");
+  //   return;
+  // }
+  // if (argv.lengh > 4) {
+  //   console.error("too many arguments");
+  //   return;
+  // }
+  // const baseURL = argv[2];
+  parseHTML("https://blog.boot.dev");
 }
 
 main();
